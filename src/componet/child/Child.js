3dc819e2a955dd1780b50b972811782f6d1122b4
Rahul-Kumar24 <ravikroy582002@gmail.com>
2021-09-18 13:56:16 +0530

    hackfleo

---
+++ src/componet/child/Child.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Child.css'
+const Child = (props) =>{
+          console.log(props.item);
+          console.log(props.item.name)
+          var data = [];
+          for ( var i=1; i<4; i++ ){
+                    data.push(props.item[i]);
+          }
+          console.log(data.length)
+          const onClickHandler = ()=>{
+                    console.log(data.name)
+          }
+          return (
+                    <div>
+                              {data.map((item)=>(
+                                        <div className="display">
+                                        <div className="card1 shadow5">
+                                        <div className='hader'>
+                                                  <h6>{item.name}</h6>
+                                                  <h6>{(item.totalsales/item.targetsales)*100}%</h6>
+                                        </div>
+                                        <div className="haderbody">
+                                                  <h6>Total Sales - {item.totalsales} Crore</h6>
+                                                  <button className='btn btn-primary' onClick={onClickHandler}>Off Track</button>
+                                        </div>
+                                        <div className="footer">
+                                                  <h6>Target Sales - {item.targetsales} Crore</h6>
+                                        </div>
+                                        </div>
+                              </div>
+                              ))}
+                    </div>
+          )
+}
+
+export default Child
---
+++ src/componet/main/Main.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import './Main.css'
+import data from '../../data/Data.json'
+import Child from '../child/Child'
+const Main = () => {
+          const [show , setShow]=useState(false);
+          const onClickHandler=()=>{
+                    setShow(true);
+                    if(show){
+                              setShow(false);
+                    }
+          }
+          
+          return (
+                    <div>                           
+                              <div className="card shadow2">
+                                        <div className='hader'>
+                                                  <h6>{data[0].name}</h6>
+                                                  <h6>{(data[0].totalsales/data[0].targetsales)*100}%</h6>
+                                        </div>
+                                        <div className="haderbody">
+                                                  <h6>Total Sales - {data[0].totalsales} Crore</h6>
+                                                  <button className={`btn  btn-primary`} onClick={onClickHandler}>Off Track</button>
+                                        </div>
+                                        <div className="footer">
+                                                  <h6>Target Sales - {data[0].targetsales} Crore</h6>
+                                        </div>
+                                        
+                              </div>
+                              <div>
+                              {show&&<Child item={data} />}
+                              </div>
+                              
+                    </div>
+          )
+}
+
+export default Main
